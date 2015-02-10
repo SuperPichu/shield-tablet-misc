b67db7a384382584e6d44662458fa9ee37b36acd
Chris Midkiff <chrismidkiff15@gmail.com>
2015-02-10 12:39:02 -0500

    Fix test.sh to build for WiFi instead of ROW

--- test.sh
+++ test.sh
@@ -10,6 +10,6 @@ make -j 10 tegra124-tn8-p1761-1270-a04-e-battery.dtb
 make -j 10
 cat arch/arm/boot/zImage arch/arm/boot/dts/tegra124-tn8-p1761-1270-a04-e-battery.dtb > ~/dev/shield-tablet/build/zImage
 cd ~/dev/shield-tablet/build
-./mkbootimg --kernel zImage --ramdisk ramdisk/row.cpio.gz -o zips/img/boot.img
+./mkbootimg --kernel zImage --ramdisk ramdisk/wifi.cpio.gz -o zips/img/boot.img
 sudo fastboot boot zips/img/boot.img
 notify-send "Kernel Build Complete"
